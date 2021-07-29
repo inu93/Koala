cb1822aee6ccfa34d832359752f887f08c2db01e
inu93 <82743280+inu93@users.noreply.github.com>
2021-07-30 00:50:08 +0700

    Update muting.py

--- AstrakoBot/modules/muting.py
+++ AstrakoBot/modules/muting.py
@@ -78,7 +78,7 @@ def mute(update: Update, context: CallbackContext) -> str:
         bot.restrict_chat_member(chat.id, user_id, chat_permissions)
         bot.sendMessage(
             chat.id,
-            f"Muted <b>{html.escape(member.user.first_name)}</b> karena muka nya kayak kontoll 🐨!",
+            f"Muted <b>{html.escape(member.user.first_name)}</b> karena berisik!",
             parse_mode=ParseMode.HTML)
         return log
 
